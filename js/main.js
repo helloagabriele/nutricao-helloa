2f5cba09c7b59dbf8ebcd3906c71b68ee708a91a
helloagabriele <133674472+helloagabriele@users.noreply.github.com>
2024-11-19 00:22:44 +0000

    Initial commit

---
+++ js/main.js
@@ -0,0 +1,6 @@
+var titulo = document.querySelector("h1");
+titulo.textContent = "Helloa nutrição";
+var subtitulo = document.querySelector("h2");
+subtitulo.textContent = "clientes cadastrados:";
+var paciente= document.querySelector(" #primeiro-paciente");
+console.log(paciente);
